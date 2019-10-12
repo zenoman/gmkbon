e8fa3f2921ac67a920fb96d09f22dbde8d5f9a2d
devsatrio <satriosuklun@gmail.com>
2019-10-12 10:49:16 +0700

    update dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\DB;
 
 use Illuminate\Http\Request;
 
@@ -23,6 +24,33 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $notalunas = DB::table('nota')->where('status','=','lunas')->count();
+        $notabelumlunas = DB::table('nota')->where('status','=','belum lunas')->count();
+        $notapengajuan = DB::table('nota')->where('status','=','pengajuan')->count();
+        $listpengajuan = DB::table('nota')
+        ->select(DB::raw('nota.*,users.username'))
+        ->leftjoin('users','users.id','=','nota.pembeli')
+        ->where('status','=','pengajuan')
+        ->orderby('nota.id','desc')
+        ->limit(10)
+        ->get();
+
+        $listpengajuanubah = DB::table('pengajuan')
+        ->select(DB::raw('pengajuan.*,users.username,detail_nota.barang'))
+        ->leftjoin('users','users.id','=','pengajuan.pembeli')
+        ->leftjoin('detail_nota','detail_nota.id','=','pengajuan.kode_barang')
+        ->where('pengajuan.konfirmasi','=','N')
+        ->orderby('pengajuan.id','desc')
+        ->limit(10)
+        ->get();
+
+        $hutang = DB::table('nota')
+        ->select(DB::raw('SUM(kekurangan) as totalkekurangan,users.username'))
+        ->leftjoin('users','users.id','=','nota.pembeli')
+        ->groupby('nota.pembeli')
+        ->orderby('totalkekurangan','desc')
+        ->get();
+
+        return view('home',['notalunas'=>$notalunas,'notabelumlunas'=>$notabelumlunas,'notapengajuan'=>$notapengajuan,'listpengajuan'=>$listpengajuan,'listpengajuanubah'=>$listpengajuanubah,'hutang'=>$hutang]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
                 <div class="col-lg-4 col-md-12 col-sm-6 col-xs-12">
                     <div class="wb-traffic-inner notika-shadow sm-res-mg-t-30 tb-res-mg-t-30">
                         <div class="website-traffic-ctn">
-                            <h2><span class="counter">500</span></h2>
+                            <h2><span class="counter">{{$notalunas}}</span></h2>
                             <p>Nota Lunas</p>
                         </div>
                     </div>
@@ -15,7 +15,7 @@
                 <div class="col-lg-4 col-md-12 col-sm-6 col-xs-12">
                     <div class="wb-traffic-inner notika-shadow sm-res-mg-t-30 tb-res-mg-t-30">
                         <div class="website-traffic-ctn">
-                            <h2><span class="counter">100</span></h2>
+                            <h2><span class="counter">{{$notabelumlunas}}</span></h2>
                             <p>Nota Belum Lunas</p>
                         </div>
                     </div>
@@ -23,7 +23,7 @@
                 <div class="col-lg-4 col-md-12 col-sm-6 col-xs-12">
                     <div class="wb-traffic-inner notika-shadow sm-res-mg-t-30 tb-res-mg-t-30 dk-res-mg-t-30">
                         <div class="website-traffic-ctn">
-                            <h2><span class="counter">80</span></h2>
+                            <h2><span class="counter">{{$notapengajuan}}</span></h2>
                             <p>Pengajuan Nota</p>
                         </div>
                     </div>
@@ -40,80 +40,32 @@
                     <div class="recent-post-wrapper notika-shadow sm-res-mg-t-30 tb-res-ds-n dk-res-ds">
                         <div class="recent-post-ctn">
                             <div class="recent-post-title">
-                                <h2>Recent Posts</h2>
+                                <h2>Pengajuan Nota</h2>
+                                <hr>
                             </div>
                         </div>
                         <div class="recent-post-items">
+                            @foreach($listpengajuan as $lp)
                             <div class="recent-post-signle rct-pt-mg-wp">
                                 <a href="#">
                                     <div class="recent-post-flex">
                                         <div class="recent-post-img">
-                                            <img src="img/post/2.jpg" alt="" />
+                                            <i class="fa fa-circle"></i>
                                         </div>
                                         <div class="recent-post-it-ctn">
-                                            <h2>Smith</h2>
-                                            <p>Nunc quis diam diamurabitur at dolor elementum, dictum turpis vel</p>
+                                            <h2>{{$lp->username}}</h2>
+                                            <p>Pengajuan Nota '<b>{{$lp->kode}}</b>'</p>
+                                            <span class="text-muted"><i class="fa fa-calendar"></i> {{$lp->tgl}}</span>
                                         </div>
                                     </div>
                                 </a>
                             </div>
+                            @endforeach
                             <div class="recent-post-signle">
-                                <a href="#">
-                                    <div class="recent-post-flex rct-pt-mg">
-                                        <div class="recent-post-img">
-                                            <img src="img/post/1.jpg" alt="" />
-                                        </div>
-                                        <div class="recent-post-it-ctn">
-                                            <h2>John Deo</h2>
-                                            <p>Nunc quis diam diamurabitur at dolor elementum, dictum turpis vel</p>
-                                        </div>
-                                    </div>
-                                </a>
-                            </div>
-                            <div class="recent-post-signle">
-                                <a href="#">
-                                    <div class="recent-post-flex rct-pt-mg">
-                                        <div class="recent-post-img">
-                                            <img src="img/post/4.jpg" alt="" />
-                                        </div>
-                                        <div class="recent-post-it-ctn">
-                                            <h2>Malika</h2>
-                                            <p>Nunc quis diam diamurabitur at dolor elementum, dictum turpis vel</p>
-                                        </div>
-                                    </div>
-                                </a>
-                            </div>
-                            <div class="recent-post-signle">
-                                <a href="#">
-                                    <div class="recent-post-flex rct-pt-mg">
-                                        <div class="recent-post-img">
-                                            <img src="img/post/2.jpg" alt="" />
-                                        </div>
-                                        <div class="recent-post-it-ctn">
-                                            <h2>Smith</h2>
-                                            <p>Nunc quis diam diamurabitur at dolor elementum, dictum turpis vel</p>
-                                        </div>
-                                    </div>
-                                </a>
-                            </div>
-                            <div class="recent-post-signle">
-                                <a href="#">
-                                    <div class="recent-post-flex rct-pt-mg">
-                                        <div class="recent-post-img">
-                                            <img src="img/post/1.jpg" alt="" />
-                                        </div>
-                                        <div class="recent-post-it-ctn">
-                                            <h2>John Deo</h2>
-                                            <p>Nunc quis diam diamurabitur at dolor elementum, dictum turpis vel</p>
-                                        </div>
-                                    </div>
-                                </a>
-                            </div>
-                            <div class="recent-post-signle">
-                                <a href="#">
+                                <a href="{{url('pengajuan/nota')}}">
                                     <div class="recent-post-flex rc-ps-vw">
                                         <div class="recent-post-line rct-pt-mg">
-                                            <p>View All</p>
+                                            <p>Lihat Semua</p>
                                         </div>
                                     </div>
                                 </a>
@@ -121,84 +73,39 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
+                <div class="col-lg-5 col-md-6 col-sm-6 col-xs-12">
                     <div class="recent-post-wrapper notika-shadow sm-res-mg-t-30 tb-res-ds-n dk-res-ds">
                         <div class="recent-post-ctn">
                             <div class="recent-post-title">
-                                <h2>Recent Posts</h2>
+                                <h2>Pengajuan Edit Nota</h2>
+                                <hr>
                             </div>
                         </div>
                         <div class="recent-post-items">
+                            @foreach($listpengajuanubah as $lpu)
                             <div class="recent-post-signle rct-pt-mg-wp">
                                 <a href="#">
                                     <div class="recent-post-flex">
                                         <div class="recent-post-img">
-                                            <img src="img/post/2.jpg" alt="" />
+                                            <i class="fa fa-circle"></i>
                                         </div>
                                         <div class="recent-post-it-ctn">
-                                            <h2>Smith</h2>
-                                            <p>Nunc quis diam diamurabitur at dolor elementum, dictum turpis vel</p>
+                                            <h2>{{$lpu->username}}</h2>
+                                            <p>Menambah <b>{{$lpu->jumlah}} Pcs </b> jumlah terjual <b>{{$lpu->barang}}</b> di nota <b>{{$lpu->kode_nota}}</b></p>
+                                            <span class="text-muted">
+                                                <i class="fa fa-calendar"></i> {{$lpu->tgl}}
+                                                <i class="fa fa-clock-o"></i> {{$lpu->jam}} 
+                                            </span>
                                         </div>
                                     </div>
                                 </a>
                             </div>
+                            @endforeach
                             <div class="recent-post-signle">
-                                <a href="#">
-                                    <div class="recent-post-flex rct-pt-mg">
-                                        <div class="recent-post-img">
-                                            <img src="img/post/1.jpg" alt="" />
-                                        </div>
-                                        <div class="recent-post-it-ctn">
-                                            <h2>John Deo</h2>
-                                            <p>Nunc quis diam diamurabitur at dolor elementum, dictum turpis vel</p>
-                                        </div>
-                                    </div>
-                                </a>
-                            </div>
-                            <div class="recent-post-signle">
-                                <a href="#">
-                                    <div class="recent-post-flex rct-pt-mg">
-                                        <div class="recent-post-img">
-                                            <img src="img/post/4.jpg" alt="" />
-                                        </div>
-                                        <div class="recent-post-it-ctn">
-                                            <h2>Malika</h2>
-                                            <p>Nunc quis diam diamurabitur at dolor elementum, dictum turpis vel</p>
-                                        </div>
-                                    </div>
-                                </a>
-                            </div>
-                            <div class="recent-post-signle">
-                                <a href="#">
-                                    <div class="recent-post-flex rct-pt-mg">
-                                        <div class="recent-post-img">
-                                            <img src="img/post/2.jpg" alt="" />
-                                        </div>
-                                        <div class="recent-post-it-ctn">
-                                            <h2>Smith</h2>
-                                            <p>Nunc quis diam diamurabitur at dolor elementum, dictum turpis vel</p>
-                                        </div>
-                                    </div>
-                                </a>
-                            </div>
-                            <div class="recent-post-signle">
-                                <a href="#">
-                                    <div class="recent-post-flex rct-pt-mg">
-                                        <div class="recent-post-img">
-                                            <img src="img/post/1.jpg" alt="" />
-                                        </div>
-                                        <div class="recent-post-it-ctn">
-                                            <h2>John Deo</h2>
-                                            <p>Nunc quis diam diamurabitur at dolor elementum, dictum turpis vel</p>
-                                        </div>
-                                    </div>
-                                </a>
-                            </div>
-                            <div class="recent-post-signle">
-                                <a href="#">
+                                <a href="{{url('pengajuan/editnota')}}">
                                     <div class="recent-post-flex rc-ps-vw">
                                         <div class="recent-post-line rct-pt-mg">
-                                            <p>View All</p>
+                                            <p>Lihat Semua</p>
                                         </div>
                                     </div>
                                 </a>
@@ -206,53 +113,35 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
+                <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
                     <div class="recent-items-wp notika-shadow sm-res-mg-t-30">
                         <div class="rc-it-ltd">
                             <div class="recent-items-ctn">
                                 <div class="recent-items-title">
-                                    <h2>Recent Items</h2>
+                                    <h2>Hutang Terbanyak</h2>
+                                    <hr>
                                 </div>
                             </div>
                             <div class="recent-items-inn">
                                 <table class="table table-inner table-vmiddle">
                                     <thead>
                                         <tr>
-                                            <th>ID</th>
-                                            <th>Name</th>
-                                            <th style="width: 60px">Price</th>
+                                            <th>Username</th>
+                                            <th style="text-align: right;">Total</th>
                                         </tr>
                                     </thead>
                                     <tbody>
+                                        @foreach($hutang as $htg)
                                         <tr>
-                                            <td class="f-500 c-cyan">4555</td>
-                                            <td>Samsung Galaxy Mega</td>
-                                            <td class="f-500 c-cyan">$921</td>
+                                            <td>{{$htg->username}}</td>
+                                            <td align="right">
+                                                <b>{{"Rp ". number_format($htg->totalkekurangan,0,',','.')}}</b>
+                                            </td>
                                         </tr>
-                                        <tr>
-                                            <td class="f-500 c-cyan">4556</td>
-                                            <td>Huawei Ascend P6</td>
-                                            <td class="f-500 c-cyan">$240</td>
-                                        </tr>
-                                        <tr>
-                                            <td class="f-500 c-cyan">8778</td>
-                                            <td>HTC One M8</td>
-                                            <td class="f-500 c-cyan">$400</td>
-                                        </tr>
-                                        <tr>
-                                            <td class="f-500 c-cyan">5667</td>
-                                            <td>Samsung Galaxy Alpha</td>
-                                            <td class="f-500 c-cyan">$870</td>
-                                        </tr>
-                                        <tr>
-                                            <td class="f-500 c-cyan">7886</td>
-                                            <td>LG G3</td>
-                                            <td class="f-500 c-cyan">$790</td>
-                                        </tr>
+                                        @endforeach
                                     </tbody>
                                 </table>
                             </div>
-              <div id="recent-items-chart" class="flot-chart-items flot-chart vt-ct-it tb-rc-it-res"></div>
                         </div>
                     </div>
                 </div>
